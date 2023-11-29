70309a7143755470d34df0e88249be17407ed6e6
vishal <vishal.dudalkar@infanion.com>
2023-11-29 13:44:49 +0530

    changes

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('/testing', function () {
     return response()->json([
-        'message' => 'Test API.'
+        'message' => 'Identity manager.'
     ]);
 });
 
